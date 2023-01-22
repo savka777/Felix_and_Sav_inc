5af137e472246262a035cf6aea174d02d88b53d0
Felixs123 <101900450+Felixs123@users.noreply.github.com>
2023-01-21 21:23:37 -0500

    added enemy prefab

--- FinalProjectUnity2/Assets/Scripts/Enemy.cs
+++ FinalProjectUnity2/Assets/Scripts/Enemy.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator;
+    GameObject player;
+    [SerializeField] private bool isGround;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    private Vector3 velocity;
+    [SerializeField] private float gravity;
     void Start()
     {
-        
+        player = GameObject.Find("C02");
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsWalking", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direction = (player.transform.position) - (gameObject.transform.position);
+        direction.Normalize();
+        transform.position += direction *3* Time.deltaTime;
+        Vector3 minDirection = new  Vector3(0, direction.y, 0);
+        transform.rotation = Quaternion.LookRotation(direction-(minDirection));
+        isGround = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        //once player is grounded, stop the gravity
+        if (isGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
     }
 }
