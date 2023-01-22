7c917ba0f9171ae1d560c13c441039ced9e11c36
Felixs123 <101900450+Felixs123@users.noreply.github.com>
2023-01-22 18:28:37 -0500

    got the player to stay grounded

--- FinalProjectUnity2/Assets/Scripts/Enemy.cs
+++ FinalProjectUnity2/Assets/Scripts/Enemy.cs
@@ -6,33 +6,42 @@ public class Enemy : MonoBehaviour
 {
     Animator animator;
     GameObject player;
-    /*[SerializeField] private bool isGround;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private LayerMask groundMask;
-    private Vector3 velocity;
-    [SerializeField] private float gravity;*/
-    void Start()
-    {
-        player = GameObject.Find("C02");
-        animator = GetComponent<Animator>();
-        animator.SetBool("IsWalking", true);
-    }
+    //gravity code
+    /*
+[SerializeField] private bool isGround;
+[SerializeField] private float groundCheckDistance;
+[SerializeField] private LayerMask groundMask;
+private Vector3 velocity;
+[SerializeField] private float gravity;*/
+void Start()
+{
+    player = GameObject.Find("C02");
+    animator = GetComponent<Animator>();
+    animator.SetBool("IsWalking", true);
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 direction = (player.transform.position) - (gameObject.transform.position);
-        direction.Normalize();
-        transform.position += direction *3* Time.deltaTime;
-        Vector3 minDirection = new  Vector3(0, direction.y, 0);
-        transform.rotation = Quaternion.LookRotation(direction-(minDirection));
-        /*isGround = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+// Update is called once per frame
+void Update()
+{
+    Vector3 noFly = new Vector3(player.transform.position.x,gameObject.transform.position.y,player.transform.position.z);
+    Vector3 direction = (noFly) - (gameObject.transform.position);
+    direction.Normalize();
+    transform.position += direction *3* Time.deltaTime;
+    Vector3 minDirection = new  Vector3(0, direction.y, 0);
+    transform.rotation = Quaternion.LookRotation(direction-(minDirection));
 
-        //once player is grounded, stop the gravity
-        if (isGround && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        velocity.y += gravity * Time.deltaTime;*/
+
+
+
+    //gravity code
+    /*
+    isGround = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+    //once player is grounded, stop the gravity
+    if (isGround && velocity.y < 0)
+    {
+        velocity.y = -2f;
     }
+    velocity.y += gravity * Time.deltaTime;*/
+}
 }
