9482a9f7afd7a721a1ef3de2a8aeddadfb547e72
savka777 <sav.bojko@gmail.com>
2023-01-25 15:21:14 -0500

    Update Scene1

--- FinalProjectUnity2/Assets/Scripts/Enemy.cs
+++ FinalProjectUnity2/Assets/Scripts/Enemy.cs
@@ -4,21 +4,90 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    GameObject player;
     Animator animator;
+    GameObject player;
+    [SerializeField] Vector3 target1;
+    [SerializeField] Vector3 target2;
+
+    CharacterController character;
+    Vector3 direction;
+
+    //Jumping code
+
+    float initialJumpVelocity;
+    float maxJumpHeight = 800.0f;
+
+    [SerializeField] float jumpDistance = 10.0f;
     void Start()
     {
         player = GameObject.Find("C02");
         animator = GetComponent<Animator>();
-        animator.SetBool("IsWalking", false);
+        animator.SetBool("IsWalking", true);
+        character = GetComponent<CharacterController>();
+        target1.y = gameObject.transform.position.y;
+        target2.y = gameObject.transform.position.y;
+        StartCoroutine(ranJumpCoroutine());
     }
 
-   
+    // Update is called once per frame
     void Update()
     {
-        Vector3 direction = (player.transform.position) - (gameObject.transform.position);
+        if (Vector3.Distance(transform.position, target1) < 0.5f)
+        {
+            direction = (target2) - (gameObject.transform.position);
+        }
+        if (Vector3.Distance(transform.position, target2) < 0.5f)
+        {
+            direction = (target1) - (gameObject.transform.position);
+        }
+        if (Vector3.Distance(gameObject.transform.position, player.transform.position) < 5f)
+        {
+            direction = (player.transform.position) - (gameObject.transform.position);
+        }
+
+
+
+
         direction.Normalize();
-        transform.position += direction * Time.deltaTime;
-        transform.rotation = Quaternion.LookRotation(direction);
+
+        character.Move(direction * Time.deltaTime * 3);
+        Vector3 minDirection = new Vector3(0, direction.y, 0);
+        transform.rotation = Quaternion.LookRotation(direction - (minDirection));
+
+
+
+
+
+    }
+
+
+
+
+    IEnumerator ranJumpCoroutine()
+    {
+        do
+        {
+            float currentHight = transform.position.y;
+            int ran = Random.Range(1, 11);
+            int i = 0;
+            yield return new WaitForSeconds(ran);
+            do
+            {
+                character.Move(Vector3.up * maxJumpHeight * 0.1f * Time.deltaTime);
+                yield return new WaitForSeconds(0.01f);
+                i++;
+            } while (i <= 10);
+            yield return new WaitForSeconds(5f);
+
+        } while (true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        AnimationMovementController animovement = other.GetComponent<AnimationMovementController>();
+        if (animovement)
+        {
+            animator.SetBool("IsPunch", true);
+        }
+
     }
 }
