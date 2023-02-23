434e5c1a6605bfb741d785ca485c8583542c8d64
savka777 <sav.bojko@gmail.com>
2023-02-23 17:26:32 -0500

    Timer Updated

--- FinalProjectUnity2/Assets/Scripts/Timer.cs
+++ FinalProjectUnity2/Assets/Scripts/Timer.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
     private float startTime;
     private bool timerIsRunning = false;
+    private float countdownTime = 300f; // 5 minutes in seconds
 
-    private void Start()
+private void Start()
     {
-        // Set the start time when the scene loads
-        startTime = Time.time;
+        // Set the start time to 5 minutes when the scene loads
+        startTime = Time.time + countdownTime;
         timerIsRunning = true;
     }
 
@@ -20,11 +26,21 @@ public class Timer : MonoBehaviour
     {
         if (timerIsRunning)
         {
-            // Calculate the time elapsed since the timer started and display it
-            float elapsedTime = Time.time - startTime;
-            string minutes = ((int)elapsedTime / 60).ToString("00");
-            string seconds = (elapsedTime % 60).ToString("00");
-            timerText.text = $"{minutes}:{seconds}";
+            // Calculate the time remaining in the countdown and display it
+            float timeRemaining = startTime - Time.time;
+            if (timeRemaining <= 0f)
+            {
+                // If the countdown has reached 0, stop the timer and display "00:00"
+                timerIsRunning = false;
+                timerText.text = "00:00";
+            }
+            else
+            {
+                // Convert the time remaining to minutes and seconds and display it
+                string minutes = ((int)timeRemaining / 60).ToString("00");
+                string seconds = (timeRemaining % 60).ToString("00");
+                timerText.text = $"{minutes}:{seconds}";
+            }
         }
     }
 
